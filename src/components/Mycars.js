281966170d385ce9d00bc3f4977fad61729424fc
clemsemifir <clementine@semifir.com>
2020-03-11 18:08:05 +0100

    Initial commit

--- src/components/Mycars.js
+++ src/components/Mycars.js
@@ -9,9 +9,14 @@ class Mycars extends Component {
             <div>
                 {/* <h1>Catalogue de voiture </h1> */}
                 <h1>{this.props.title}</h1>
-                    <Car color='red'>FORD</Car>
-                    <Car>TESLA</Car> 
-                    <Car color='dark'></Car>
+                    <Car color='red'>FORD</Car><button>Suprimmer</button>
+                    <Car>TESLA</Car><button>Suprimmer</button>
+                    <Car color='dark'></Car><button>Suprimmer</button>
+
+                    <div>
+                    <input type="text" placeholder="Ajouter une voiture"></input>
+                        <button>Ajouter</button>
+                    </div>
             </div>
         )
     }
